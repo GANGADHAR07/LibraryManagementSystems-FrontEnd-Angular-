566f1324c119393046fba1ea3bb0de6991fbbc57
Gangadhar <k.gangadhar@dxc.com>
2020-10-17 10:01:39 +0530

    First Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddBookComponent } from './add-book/add-book.component';
+import { BooksListComponent } from './books-list/books-list.component';
+import { SearchBookComponent } from './search-book/search-book.component';
+import { UpdateBooksComponent } from './update-books/update-books.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : "add-Book" , component : AddBookComponent
+  },
+  {
+    path : "search-Book" , component : SearchBookComponent
+  },
+  {
+    path : "books", component : BooksListComponent
+  },
+  {
+    path: "update-Books/:title", component: UpdateBooksComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BooksListComponent } from './books-list/books-list.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { UpdateBooksComponent } from './update-books/update-books.component';
+import { AddBookComponent } from './add-book/add-book.component';
+import { SearchBookComponent } from './search-book/search-book.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BooksListComponent,
+    UpdateBooksComponent,
+    AddBookComponent,
+    SearchBookComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
